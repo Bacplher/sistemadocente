bb48fd10b5482c426a8d706cf4544de3379321aa
Hernan Bacalla <hernanbacalla@gmail.com>
2014-10-16 21:15:46 -0500

    cambio de posicion

--- modelo/modelo.php
+++ modelo/modelo.php
@@ -1,1 +1,28 @@
 <?php
+//import the class
+require_once '../configuracion/db_conn.class.php';
+
+//save connection object to variable.
+$db = dbConn::getConnection();
+
+//query database using prepared statement
+//sql
+$datos = $db->query('SELECT * FROM docente');
+
+//save result of sql query to variable.
+//$result = $sql->execute( array(':id' => 'SARA MARTINA') );
+
+
+
+//echo result
+$datos->setFetchMode(PDO::FETCH_ASSOC);
+ echo "<table border='1'>";
+    while($result = $datos->fetch()){
+        echo "<tr>";
+        foreach ($result as $col_value) {
+            echo "<td>$col_value</td>";
+        }
+        echo "</tr>";
+
+    }
+	echo "</table>";
