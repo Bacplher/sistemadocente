f90024f1e5cbb4cd19c8343dff295cc9fca9f297
Hernan Bacalla <hernanbacalla@gmail.com>
2014-11-05 15:20:47 -0500

    Prueven el login Usuario:Nombre Contraseña:DNI

--- login.php
+++ login.php
@@ -3,22 +3,25 @@ require_once '/basedatos/db_conn.class.php';
 $nombre = $_POST['nombre'];
 $dni = $_POST['dni'];
 
+
 //save connection object to variable.
 $db = dbConn::getConnection();
 
 //query database using prepared statement
 //sql
-$sql1 = $db->query('SELECT Nombre FROM docente WHERE Nombre="$nombre"');
-$sql2 = $db->query('SELECT Dni FROM docente WHERE Nombre="$dni" ');
+$sql1 = $db->prepare( "SELECT Dni FROM docente WHERE Dni LIKE '$dni'");
+$sql2 = $db->prepare( "SELECT Nombre FROM docente WHERE Nombre LIKE '$nombre'");
 
 //save result of sql query to variable.
-$result1 = $sql1->execute();
-$result2 = $sql2->execute();
-if ($result1==$nombre AND $result2==$dni) {
-    header('Location: modelo/modelo.php');
-}
-else{
+$resultado1 = $sql1->execute();
+$resultado2 = $sql2->execute();
+$row1 = $sql1->fetch();
+$row2 = $sql2->fetch();
+
+if ($row1[0]==null or $row2[0]==null or $row1[0]<>$dni or $row2[0]<>$nombre ){
     header('Location: index.html');
+}else{
+    header('Location: modelo/modelo.php');
 }
 /*
 $nombre = $_POST['nombre'];
--- vista/layout/default/js/index.js
+++
@@ -1,8 +0,0 @@
-
-$('.gn-icon-menu').hover(function(){
-  $('.gn-menu-wrapper').toggleClass('gn-open-part');
-});
-
-$('.gn-menu-wrapper').hover(function(){
-  $(this).toggleClass('gn-open-all');
-})
